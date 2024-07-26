aaebf4918a8f93abba2c631981b760f3e29319fe
Elberte Plínio <elberte.dev@gmail.com>
2024-07-26 00:55:17 +0000

    atualizado mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Nunca se canse de melhorar e evoluir!</h1>;
+  return (
+    <h1>
+      Rafa eu gosto muito de você, se você gosta de mim, dá uma risadinha!
+    </h1>
+  );
 }
 
 export default Home;
