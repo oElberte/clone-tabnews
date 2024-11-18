37d10be0f54a6c98489069c9eed67f1e86b472ed
Elberte Plínio <dev@elberte.com>
2024-11-18 13:47:14 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,21 +6,16 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to '/api/v1/migrations' should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response1.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const response1Body = await response1.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
-
-  // Run the migrations again
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
